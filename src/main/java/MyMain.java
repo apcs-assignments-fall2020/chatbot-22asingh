9db8d9287d2fc9bb82e88100771754eb365a2af4
22asingh <22asingh@athenian.org>
2020-09-02 03:59:31 +0000

    Added all 5 chatbot questions to main

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        System.out.println("What is your name? ");
+        String name = scan.next();
+        System.out.println("Hello " + name + ". It's nice to meet you.");
+        System.out.println("What is your favorite color, " + name + "? ");
+        String color = scan.next();
+        System.out.println("Cool, I like " + color + " as well!");
+        System.out.println("What is your favorite number? ");
+        int number = Integer.parseInt(scan.next());
+        System.out.println("Oh " + number + " is my favorite number too!");
+        System.out.println("What is your favorite movie, " + name + "? ");
+        String movie = scan.next();
+        System.out.println("Wow! I love " + movie + " too!");
+        System.out.println("What is your favorite sport to watch? ");
+        String sport = scan.next();
+        System.out.println("I like watching " + sport + " too!");
         scan.close();
     }
 }
